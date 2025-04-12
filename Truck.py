90aa05d39af82964423ab51f8a4128bc93459f2f
PretzelPink <156927876+PretzelPink@users.noreply.github.com>
2025-04-11 17:20:32 -0700

    added truck functionality, unable to retrieve stored packages. completed destination search function

--- Truck.py
+++ Truck.py
@@ -1,4 +1,14 @@
-class Truck:
+class truck:
     def __init__(self):
-        print("new truck")
+        self.packages = []  # Instance variable: each truck has its own package list
+        self.travelSpeed = 18
+        self.currentLocation = "4001 South 700 East"
 
+    def loadPackages(self, packageList):
+        i = 0
+        for x in packageList:
+            if (((i < 16)) and (len(packageList) > 0)):
+                self.packages.append(x)
+                i += 1
+            else:
+                break
